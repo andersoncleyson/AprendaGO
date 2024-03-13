e73e9cfd56ec2ce5794b5c397a970da6ebdd240e
Elliot Anderson <andersoncleyson@protonmail.com>
2024-03-13 07:04:51 -0300

    unmarshal

--- Cap16_JSON/unmarshal.go
+++ Cap16_JSON/unmarshal.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
+type pessoa struct {
+	Nome          string
+	Sobrenome     string
+	Idade         int
+	Profissao     string
+	Contabancaria float64
+}
+
+func main() {
+	jamesbond := pessoa{
+		Nome:          "James",
+		Sobrenome:     "Bond",
+		Idade:         40,
+		Profissao:     "Agente Secreto",
+		Contabancaria: 10000.50,
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.Encode(jamesbond)
+}
